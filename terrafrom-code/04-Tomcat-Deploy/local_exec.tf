8792274d9be1632005906055e224ada70f87ff5b
Kiba87 <38249285+Kiba87@users.noreply.github.com>
2020-05-06 16:06:23 +0530

    commenting

--- terrafrom-code/04-Tomcat-Deploy/local_exec.tf
+++ terrafrom-code/04-Tomcat-Deploy/local_exec.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "local" {
-    path = "/tmp/terraform/workspace/terraform.tfstate"
-  }
+#terraform {
+ # backend "local" {
+  #  path = "/tmp/terraform/workspace/terraform.tfstate"
+  #}
 
-}
+#}
 
 provider "aws" {
   region = "us-east-1"
